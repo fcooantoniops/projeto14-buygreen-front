db25506d75c088526724ea2d5f40875b5dea8ac7
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-15 05:32:42 -0300

    feat: login styled-components

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,9 +1,16 @@
-
-
+import { AuthBackground, Logo, Form, Button, ButtonText, SignUpLink} from '../../styles/auth.js';
 
 export default function Login() {
 
     return (
-        <div>HelloWorld</div>
+        <AuthBackground>
+            <Logo>BuyGreen</Logo>
+            <Form placeholder="Email"></Form>
+            <Form placeholder="Senha"></Form>
+            <Button>
+                <ButtonText>Entrar</ButtonText>
+            </Button>
+            <SignUpLink>Ainda não é cadastrado? Cadastre-se agora!</SignUpLink>
+        </AuthBackground>
     )
 }
