616158989243ce7c30f64de8898c68203cb7907b
Daivison Morais <daivison13pinheiro@gmail.com>
2022-09-18 18:42:29 -0300

    layout, rotas get, post, delete e put, 80% implementadas

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/login/Login.js';
+import Login from "./pages/login/Login.js";
+import CartShopping from "./pages/CartShopping/CartShopping.js";
 
 export default function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path = '/login' element = { <Login /> } />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<CartShopping />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
