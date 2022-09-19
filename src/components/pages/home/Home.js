bf757c76c4faf59f5761b3d55bf44c6f48388742
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-19 16:27:16 -0300

    feat: home redirects inside an useState hook

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,20 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import { TailSpin } from "react-loader-spinner";
 import { AuthBackground } from "../../styles/authStyles";
+import { useEffect } from "react";
 
 export default function Home(){
 
     const navigate = useNavigate()
     const token = localStorage.getItem("token");
 
-    function autoLogin() {
+    // function autoLogin() {
+    //     if(token) {
+    //         navigate('/products');
+    //     } else {
+    //         navigate('/login');
+    //     }
+    // }
+    // autoLogin()
+
+    useEffect(() => {
         if(token) {
             navigate('/products');
         } else {
             navigate('/login');
         }
-    }
-    autoLogin()
+    });
     
 
     return (<AuthBackground>
